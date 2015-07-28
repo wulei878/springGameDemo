292da067551d92b0de06f95afcb0f056d7e24677
吴磊 <lei.wu3@renren-inc.com>
2015-07-28 10:08:21 +0800

    Initial Commit

---
+++ ZhanYeZhuShou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZhanYeZhuShou
+//
+//  Created by Owen on 15/7/28.
+//  Copyright (c) 2015年 Owen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
